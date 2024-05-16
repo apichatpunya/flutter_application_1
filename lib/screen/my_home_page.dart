96328136f869eade6ff818dacdab1aee52ce1782
Jsoft <apichat.punya@gmail.com>
2024-05-16 14:03:09 +0700

    update

---
+++ lib/screen/my_home_page.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to My App',
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Get Started'),
+            ),
+            SizedBox(height: 20),
+            Card(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  'This is a sample card',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
